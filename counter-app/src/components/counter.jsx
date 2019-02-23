e71740abf1269db18b446a1aab1f9ce229e266c0
Piyush Ghate <piyushghate17@gmail.com>
2019-02-23 15:13:40 +0530

    handling increments

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: []
+    count: 0
   };
 
   //   styles = {
@@ -11,25 +10,40 @@ class Counter extends Component {
   //     fontWeight: "bold"
   //   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>there are no tags!</p>;
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  render() {
+    let classes = this.getBadgeClasses();
 
     return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
-  render() {
-    return (
-      <div>
-        {this.state.tags.length === 0 && "please create a new tag"}
-        {this.renderTags()}
-      </div>
-    );
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    // return this.state.count === 0 ? "Zero" : this.state.count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
